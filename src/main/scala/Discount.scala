300e14a2f1f74c3f16c4a819908f5a599bd7bb38
krzyswys <krzyswys3@gmail.com>
2023-06-05 14:18:01 +0200

    added User class

--- src/main/scala/Discount.scala
+++ src/main/scala/Discount.scala
@@ -1,5 +1,7 @@
 enum Discount(val discointValue: Double):
-  case EMERYT extends Discount(0.02)
-  case UCZEN extends Discount(0.05)
+  case SENIOR extends Discount(0.02)
+  case ACADEMIC extends Discount(0.05)
   case STUDENT extends Discount(0.03)
+  case FULL extends Discount(0.00)
+  case DISABILITY extends Discount(0.10)
 
