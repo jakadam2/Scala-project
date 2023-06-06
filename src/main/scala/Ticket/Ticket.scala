2fc12808993ed34e657fc03452de4d8cb2da0894
jakadam <adamwozny7@gmail.com>
2023-06-06 11:47:29 +0200

    ending a ticket methods

--- src/main/scala/Ticket/Ticket.scala
+++ src/main/scala/Ticket/Ticket.scala
@@ -5,7 +5,7 @@ import java.util.Calendar
 
 class Ticket(val discount:Discount,// who is let to use ticket
              val nrOfTicket:Int,//  inner ordering number of ticket
-             val ticketTime:Int,//  how much seconds left to out dating
+             val ticketTime:dateStruct,//  how much seconds left to out dating
              val timeOfBuy:dateStruct,// time when tickets was bought
              val routeNumber:String,//
              var user:User,
@@ -14,11 +14,13 @@ class Ticket(val discount:Discount,// who is let to use ticket
             )
 {
   def timeLeft(): dateStruct={
-    return dateStruct(0,0,0)
+    val now = Calendar.getInstance()
+    val nowStruct = dateStruct(now.get(Calendar.PM),now.get(Calendar.MINUTE),now.get(Calendar.SECOND))
+    val curDiff = nowStruct.subtract(timeOfBuy)
+    ticketTime.subtract(curDiff)
   }
   def isValid(): Boolean = {
-    
-      true
+      timeLeft().hour >= 0 && timeLeft().seconds >= 0 && timeLeft().minute >= 0
   }
   
 
